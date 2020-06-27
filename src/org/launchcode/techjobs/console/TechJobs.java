85f9fc46073a5ac7dc31006e1d8ba14947875d4f
Jhoan Peguero <jpeguero1989@gmail.com>
2020-06-26 22:25:29 -0400

    Done

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -58,10 +59,11 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term: ");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toLowerCase();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
+                    //System.out.println("Search all fields not yet implemented.");
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +112,19 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+if (someJobs.size() == 0)
+{
+    System.out.println("No jobs found");
+}
+else {
+    for (HashMap<String, String> value : someJobs) {
+        System.out.println("********");
+        for (Map.Entry<String, String> current : value.entrySet()) {
+            System.out.println(current.getKey() + ": " + current.getValue());
+        }
+    }
+    System.out.println("********");
+    //System.out.println("printJobs is not implemented yet");
+}
     }
 }
